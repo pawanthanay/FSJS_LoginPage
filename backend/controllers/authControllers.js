b01bbd31736752504ab7db426f5eeae57d792f3e
unknown <npawanthanay@gmail.com>
2023-02-22 23:34:49 +0530

    login route added

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -10,7 +10,6 @@ exports.home = (req, res) => {
 exports.register = async (req, res) => {
     try {
 
-        
         //collect all information
         const { firstname, lastname, email, password } = req.body;
         //validate the data, if exists
@@ -65,42 +64,23 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
 
     try {
-        //collected information from frontend
-        const {email, password} = req.body
-        //validate
-        if (!(email && password)) {
-            return res.status(401).send("email and password is required")
+       
+        // Find the user in the database
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(400).json({ success: false, message: 'Invalid email or password' });
         }
-
-        //check user in database
-        const user = await User.findOne({email})
-        //if user does not exists - assignment
-        if (!(user)) 
-        {
-            return res.status(401).send("user email does not exists")
-        } 
          
-        //match the password
-        if (user && (await bcrypt.compare(password, user.password))) {
-            const token = jwt.sign({id: user._id, email}, 'pawan', {expiresIn: '2h'})
-
-            
-            user.password = undefined
-            user.token = token
-
-            const options = {
-                expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
-                httpOnly: true
-            }
-            res.status(200).cookie("token", token, options).json({
-                success: true,
-                token,
-                user
-            })
-
+        // Check the password
+        const isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(400).json({ success: false, message: 'Invalid email or password' });
         }
-        //create token and send
-        return res.sendStatus(400).send("email or password is incorrect")
+
+         // Create and send the token
+        const token = jwt.sign({ _id: user._id }, 'pawan');
+        res.header('auth-token', token).json({ success: true, message: 'Login successful', token: token });
+
     }
     catch (error) {
         console.log(error);
